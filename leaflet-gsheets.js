cdc504a5a6004b4c7d7804ee2c88eb171d1bc9ba
jhnar <jhnar@host.docker.internal>
2020-06-09 20:05:00 +0300

    .

--- leaflet-gsheets.js
+++ leaflet-gsheets.js
@@ -11,7 +11,7 @@ function init() {
 
   // these URLs come from Google Sheets 'shareable link' form
   // the first is the polygon layer and the second the points
-  var polyURL =""https://docs.google.com/spreadsheets/d/e/2PACX-1vRAVgIyH3Tcfu9qRKULUlhMUVhy3EXNTV2-brnA9PTZDvLtu-f24y6BgSt_Ciqj_sWJdNALVqsjcSEy/pubhtml"
+  var polyURL ="https://docs.google.com/spreadsheets/d/e/2PACX-1vRAVgIyH3Tcfu9qRKULUlhMUVhy3EXNTV2-brnA9PTZDvLtu-f24y6BgSt_Ciqj_sWJdNALVqsjcSEy/pubhtml"
     //"https://docs.google.com/spreadsheets/d/1EUFSaqi30b6oefK0YWWNDDOzwmCTTXlXkFHAc2QrUxM/edit?usp=sharing";
   var pointsURL =
     "https://docs.google.com/spreadsheets/d/1kjJVPF0LyaiaDYF8z_x23UulGciGtBALQ1a1pK0coRM/edit?usp=sharing";
