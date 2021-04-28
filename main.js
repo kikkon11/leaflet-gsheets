a244e078f0aa1a5d6e4da6dd4eaca666fb489fe0
Konstantinos Kikakis <konkik@otenet.gr>
2021-04-28 11:56:01 +0300

    Update main.js

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ let panelID = "my-info-panel";
  */
 function init() {
   // Create a new Leaflet map centered on the Attica, Greece
-  var map = L.map("map").setView([37.983, 23.733], 12);
+  var map = L.map("map").setView([37.983, 23.733], 13);
 
   // This is the Carto Positron basemap
   L.tileLayer(
