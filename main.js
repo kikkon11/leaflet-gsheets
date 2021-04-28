0c7115ba1372dcae52df366cfd64eed57a161549
Konstantinos Kikakis <konkik@otenet.gr>
2021-04-28 11:48:58 +0300

    Update main.js

--- main.js
+++ main.js
@@ -23,8 +23,8 @@ let panelID = "my-info-panel";
  * init() is called when the page has loaded
  */
 function init() {
-  // Create a new Leaflet map centered on the continental US
-  map = L.map("map").setView([51.5, -0.1], 14);
+  // Create a new Leaflet map centered on the Attica, Greece
+  var map = L.map("map").setView([37.983, 23.733], 12);
 
   // This is the Carto Positron basemap
   L.tileLayer(
